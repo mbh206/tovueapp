086931bacd1d1583c173c587d6326c8d32a95136
Mark Harris <mark.harris206@gmail.com>
2025-03-17 14:39:08 +0900

    added page styling and contact form

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="h-16 bg-gray-200 flex justify-between">
+  <div class="h-20 bg-gray-300 flex justify-between">
+    <RouterLink to="/"
+      ><img
+        src="../../public/favicon.ico"
+        alt="Main site logo"
+        class="p-1 h-full animate-pulse hover:animate-none"
+    /></RouterLink>
     <div class="flex items-center">
-      <img src="../../public/favicon.ico" alt="Main site logo" class="p-2 h-full" />
-      <h1 class="text-4xl">Mark Harris</h1>
+      <div class="flex gap-3">
+        <h1 class="text-4xl">Mark<span class="text-sky-800">Harris</span></h1>
+        <h3 class="text-4xl text-white drop-shadow-sm">Portolio</h3>
+      </div>
     </div>
-    <ul class="flex gap-3 text-xl uppercase items-center mr-2">
-      <li><RouterLink to="/">Home</RouterLink></li>
-      <li><RouterLink to="/projects">Projects</RouterLink></li>
-      <li><RouterLink to="/contact">Contact</RouterLink></li>
+    <ul class="flex flex-col text-lg uppercase items-center mr-2">
+      <li class="hover:text-sky-600 p-0 m-0 text-xl/6"><RouterLink to="/">Home</RouterLink></li>
+      <li class="hover:text-sky-600 p-0 m-0 text-xl/6">
+        <RouterLink to="/projects">Projects</RouterLink>
+      </li>
+      <li class="hover:text-sky-600 p-0 m-0 text-xl/6">
+        <RouterLink to="/contact">Contact</RouterLink>
+      </li>
     </ul>
   </div>
 </template>
--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,4 +1,16 @@
 <script setup lang="ts"></script>
 <template>
-  <p>Hello from the Contact page</p>
+  <div class="w-screen h-screen bg-white">
+    <div class="flex flex-col w-96 m-auto mt-40">
+      <label for="email">Enter email:</label>
+      <input type="email" placeholder="Email" class="px-2 py-1 rounded bg-gray-200 mb-3" />
+      <label for="name">Enter name:</label>
+      <input type="text" placeholder="Name" class="px-2 py-1 rounded bg-gray-200 mb-3" />
+      <label for="message">Enter your message below:</label>
+      <textarea class="h-24 px-2 py-1 rounded bg-gray-200 mb-3"></textarea>
+      <button type="submit" class="text-white bg-blue-500 px-3 py-2 rounded hover:bg-blue-400">
+        Submit
+      </button>
+    </div>
+  </div>
 </template>
